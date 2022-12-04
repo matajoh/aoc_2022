141535596cdc71876d14d46314cd48bf373cb298
Matthew Johnson <matjoh@microsoft.com>
2022-12-04 21:36:10 +0000

    Prep for tomorrow
    Signed-off-by: Matthew Johnson <matjoh@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 mod utils;
 
 use std::env;
@@ -11,13 +12,22 @@ fn run_all() {
     day02::run();
     day03::run();
     day04::run();
+    day05::run();
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 && args[1] == "all" {
-        run_all();
+    if args.len() == 2 {
+        match args[1].as_str() {
+            "1" => day01::run(),
+            "2" => day02::run(),
+            "3" => day03::run(),
+            "4" => day04::run(),
+            "5" => day05::run(),
+            "all" => run_all(),
+            _ => println!("Unrecognized option: {}", args[1]),
+        }
     } else {
-        day04::run();
+        run_all()
     }
 }
