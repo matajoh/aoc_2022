7fc67824a35b5efedac83a441194b38c86a17ff5
Matthew A Johnson <matjoh@microsoft.com>
2022-12-16 15:20:36 +0000

    Adding some efficiency
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- src/day16.rs
+++ src/day16.rs
@@ -1,8 +1,75 @@
-use super::utils::read_to_vec;
+use crate::utils::read_to_vec;
+
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::time::Instant;
+
+#[derive(Eq, PartialEq)]
+struct PathState {
+    position: usize,
+    cost: usize,
+}
+
+impl Ord for PathState {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for PathState {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn min_path(valves: &Vec<Valve>, start: usize, end: usize) -> usize {
+    let mut dist: HashMap<usize, usize> = HashMap::new();
+    let mut prev: HashMap<usize, usize> = HashMap::new();
+    let mut vertices: HashSet<usize> = HashSet::new();
+    let mut min_heap = BinaryHeap::new();
+    for i in 0..valves.len() {
+        dist.insert(i, usize::MAX);
+        vertices.insert(i);
+    }
+
+    dist.insert(start, 0);
+    min_heap.push(PathState {
+        position: start,
+        cost: 0,
+    });
+    while let Some(state) = min_heap.pop() {
+        let u = &state.position;
+        vertices.remove(u);
+        if dist[u] == usize::MAX {
+            break;
+        }
+
+        for v in &valves[*u].leads_to {
+            let alt = dist[u] + 1;
+            if alt < dist[v] {
+                dist.insert(*v, alt);
+                prev.insert(*v, *u);
+                min_heap.push(PathState {
+                    position: *v,
+                    cost: alt,
+                })
+            }
+        }
+    }
+
+    let mut length = 0;
+    let mut current = end;
+    while current != start {
+        current = prev[&current];
+        length += 1;
+    }
+    length
+}
 
 #[derive(Debug)]
 struct Valve {
@@ -10,26 +77,34 @@ struct Valve {
     leads_to: Vec<usize>,
 }
 
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Hash, Copy, Clone, Eq, PartialEq)]
 struct Position {
     steps: usize,
     valve: usize,
 }
 
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Hash, Copy, Clone, Eq, PartialEq)]
 struct State {
-    unopened: HashSet<usize>,
+    unopened: u64,
     pressure_released: usize,
     pos0: Position,
     pos1: Position,
 }
 
-fn can_move_to(
-    unopened: &HashSet<usize>,
-    distances: &Vec<Vec<usize>>,
-    pos: &Position,
-) -> Vec<usize> {
-    unopened
+fn to_valves(unopened: u64) -> Vec<usize> {
+    (0..60)
+        .into_iter()
+        .filter(|s| (unopened & (1 << s)) > 0)
+        .collect()
+}
+
+fn open(unopened: u64, v0: usize, v1: usize) -> u64 {
+    let clear = ((1 << v0) | (1 << v1)) ^ u64::MAX;
+    unopened & clear
+}
+
+fn can_move_to(unopened: u64, distances: &Vec<Vec<usize>>, pos: &Position) -> Vec<usize> {
+    to_valves(unopened)
         .iter()
         .filter(|i| distances[pos.valve][**i] < pos.steps)
         .map(|i| *i)
@@ -38,8 +113,7 @@ fn can_move_to(
 
 impl State {
     fn max_outcome(&self, valves: &Vec<Valve>) -> usize {
-        let mut flows = self
-            .unopened
+        let mut flows = to_valves(self.unopened)
             .iter()
             .map(|i| valves[*i].flow_rate)
             .collect::<Vec<usize>>();
@@ -77,12 +151,9 @@ impl State {
             },
             None => Position { steps: 0, valve: 0 },
         };
+
         State {
-            unopened: self
-                .unopened
-                .difference(&HashSet::from([pos0.valve, pos1.valve]))
-                .map(|v| *v)
-                .collect(),
+            unopened: open(self.unopened, pos0.valve, pos1.valve),
             pressure_released: self.pressure_released
                 + pos0.steps * valves[pos0.valve].flow_rate
                 + pos1.steps * valves[pos1.valve].flow_rate,
@@ -92,8 +163,8 @@ impl State {
     }
 
     fn get_moves(&self, distances: &Vec<Vec<usize>>) -> (Vec<usize>, Vec<usize>) {
-        let mut moves0 = can_move_to(&self.unopened, distances, &self.pos0);
-        let mut moves1 = can_move_to(&self.unopened, distances, &self.pos1);
+        let mut moves0 = can_move_to(self.unopened, distances, &self.pos0);
+        let mut moves1 = can_move_to(self.unopened, distances, &self.pos1);
         if moves0.len() == 1 && moves1.len() == 1 && moves0[0] == moves1[0] {
             let dist0 = distances[self.pos0.valve][moves0[0]];
             let dist1 = distances[self.pos1.valve][moves0[0]];
@@ -154,72 +225,6 @@ fn read_valves() -> Vec<Valve> {
         .collect()
 }
 
-#[derive(Eq, PartialEq)]
-struct PathState {
-    position: usize,
-    cost: usize,
-}
-
-impl Ord for PathState {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .cost
-            .cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
-    }
-}
-
-impl PartialOrd for PathState {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-// TODO this can be abstracted out into its own module
-fn min_path(valves: &Vec<Valve>, start: usize, end: usize) -> usize {
-    let mut dist: HashMap<usize, usize> = HashMap::new();
-    let mut prev: HashMap<usize, usize> = HashMap::new();
-    let mut vertices: HashSet<usize> = HashSet::new();
-    let mut min_heap = BinaryHeap::new();
-    for i in 0..valves.len() {
-        dist.insert(i, usize::MAX);
-        vertices.insert(i);
-    }
-
-    dist.insert(start, 0);
-    min_heap.push(PathState {
-        position: start,
-        cost: 0,
-    });
-    while let Some(state) = min_heap.pop() {
-        let u = &state.position;
-        vertices.remove(u);
-        if dist[u] == usize::MAX {
-            break;
-        }
-
-        for v in &valves[*u].leads_to {
-            let alt = dist[u] + 1;
-            if alt < dist[v] {
-                dist.insert(*v, alt);
-                prev.insert(*v, *u);
-                min_heap.push(PathState {
-                    position: *v,
-                    cost: alt,
-                })
-            }
-        }
-    }
-
-    let mut length = 0;
-    let mut current = end;
-    while current != start {
-        current = prev[&current];
-        length += 1;
-    }
-    length
-}
-
 fn distance_between(valves: &Vec<Valve>) -> Vec<Vec<usize>> {
     let mut distances = vec![vec![0; valves.len()]; valves.len()];
     for i in 0..valves.len() {
@@ -235,10 +240,19 @@ fn distance_between(valves: &Vec<Valve>) -> Vec<Vec<usize>> {
 
 fn max_pressure_released(valves: &Vec<Valve>, use_elephant: bool) -> usize {
     let mut heap = BinaryHeap::new();
+    let unopened = (0..valves.len())
+        .enumerate()
+        .filter_map(|(i, v)| {
+            if valves[v].flow_rate > 0 {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .map(|i| 1u64 << i)
+        .fold(0u64, |a, b| a | b);
     heap.push(State {
-        unopened: (0..valves.len())
-            .filter(|v| valves[*v].flow_rate > 0)
-            .collect(),
+        unopened,
         pressure_released: 0,
         pos0: Position {
             steps: if use_elephant { 26 } else { 30 },
@@ -252,17 +266,14 @@ fn max_pressure_released(valves: &Vec<Valve>, use_elephant: bool) -> usize {
 
     let distances = distance_between(valves);
 
-    // TODO
-    // 1. Need to eliminate symmetries (i.e. me and the elephant can swap places with the same outcome)
-    // 2. Idea: every time a valve is opened, recurse.
-    //    - shouldn't run out of stack
-    //    - means we can start memoizing by "remaining state". As long as key breaks symmetry, we explore far fewer paths.
+    let mut seen = HashSet::new();
 
     let mut most_pressure_released = 0;
     while let Some(current) = heap.pop() {
+        seen.insert(current);
         if current.max_outcome(valves) < most_pressure_released {
             continue;
-        } else if current.unopened.is_empty() {
+        } else if current.unopened == 0 {
             most_pressure_released = most_pressure_released.max(current.pressure_released)
         } else {
             let (moves0, moves1) = current.get_moves(&distances);
@@ -271,11 +282,17 @@ fn max_pressure_released(valves: &Vec<Valve>, use_elephant: bool) -> usize {
                 most_pressure_released = most_pressure_released.max(current.pressure_released);
             } else if moves1.is_empty() {
                 for move0 in moves0 {
-                    heap.push(current.update(valves, &distances, Some(move0), None))
+                    let next = current.update(valves, &distances, Some(move0), None);
+                    if !seen.contains(&next) {
+                        heap.push(next)
+                    }
                 }
             } else if moves0.is_empty() {
                 for move1 in moves1 {
-                    heap.push(current.update(valves, &distances, None, Some(move1)))
+                    let next = current.update(valves, &distances, None, Some(move1));
+                    if !seen.contains(&next) {
+                        heap.push(next)
+                    }
                 }
             } else {
                 for move0 in moves0 {
@@ -283,7 +300,10 @@ fn max_pressure_released(valves: &Vec<Valve>, use_elephant: bool) -> usize {
                         if move0 == *move1 {
                             continue;
                         }
-                        heap.push(current.update(valves, &distances, Some(move0), Some(*move1)))
+                        let next = current.update(valves, &distances, Some(move0), Some(*move1));
+                        if !seen.contains(&next) {
+                            heap.push(next)
+                        }
                     }
                 }
             }
@@ -304,5 +324,7 @@ pub fn run() {
     let valves = read_valves();
     println!("== Day 16 ==");
     println!("Part 1: {}", part1(&valves));
-    println!("Part 2: {}", part2(&valves))
+    let start = Instant::now();
+    println!("Part 2: {}", part2(&valves));
+    println!("{:?}", Instant::now() - start)
 }
