abae2d839d7ffca5a9897f325ac0499b1993f6fa
Matthew A Johnson <matjoh@microsoft.com>
2022-12-20 14:06:09 +0000

    Day 20
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- src/day20.rs
+++ src/day20.rs
@@ -1,58 +1,107 @@
 use crate::utils::read_to_vec;
 
-fn to_final(numbers: &Vec<i32>, indices: &Vec<i32>) -> Vec<i32> {
-    let mut result = vec![0; numbers.len()];
-    for n in 0..numbers.len() {
-        result[indices[n] as usize] = numbers[n]
+#[derive(Debug)]
+struct Node {
+    delta: i64,
+    prev: usize,
+    next: usize,
+}
+
+fn to_final(nodes: &Vec<Node>) -> Vec<i64> {
+    let mut result = vec![0];
+    let start = nodes.iter().position(|n| n.delta == 0).unwrap();
+    let mut current = nodes[start].next;
+    for _ in 1..nodes.len() {
+        result.push(nodes[current].delta);
+        current = nodes[current].next;
     }
+
     result
 }
 
-fn mix(numbers: &Vec<i32>) -> Vec<i32> {
-    let count = numbers.len() as i32;
-    let mut indices = (0..count).collect::<Vec<i32>>();
-    for n in 0..numbers.len() {
-        let index = indices[n] + numbers[n] % count;
-        let (start, end, inc) = match index {
-            _ if index >= count => (index - count, indices[n], 1),
-            _ if index < 0 => (indices[n], count + index - 1, -1),
-            _ if numbers[n] > 0 => (indices[n], index, -1),
-            _ if numbers[n] < 0 => (index, indices[n], 1),
-            _ => panic!("invalid delta"),
-        };
+fn mix(nodes: &mut Vec<Node>) {
+    let count = nodes.len() as i64 - 1;
+    for n in 0..nodes.len() {
+        let delta = nodes[n].delta % count;
+        if delta == 0 {
+            continue;
+        }
 
-        println!("{}..={} => {}", start, end, inc);
+        let mut prev = nodes[n].prev;
+        let mut next = nodes[n].next;
+        nodes[prev].next = next;
+        nodes[next].prev = prev;
 
-        for i in indices.iter_mut() {
-            if *i > start && *i <= end {
-                *i += inc
+        if delta < 0 {
+            for _ in delta..0 {
+                prev = nodes[prev].prev
+            }
+        } else {
+            for _ in 0..delta {
+                prev = nodes[prev].next
             }
         }
 
-        indices[n] = index;
-
-        for i in indices.iter_mut() {
-            if *i < 0 {
-                *i += count
-            } else if *i >= count {
-                *i -= count
-            }
-        }
-
-        println!("{:?}", to_final(numbers, &indices))
+        next = nodes[prev].next;
+        nodes[n].next = next;
+        nodes[next].prev = n;
+        nodes[n].prev = prev;
+        nodes[prev].next = n;
     }
-
-    to_final(numbers, &indices)
 }
 
-fn part1(numbers: &Vec<i32>) -> i32 {
-    let mixed = mix(numbers);
-    println!("{:?}", mixed);
-    0
+fn part1() -> i64 {
+    let mut nodes = read_nodes();
+    mix(&mut nodes);
+    let mixed = to_final(&nodes);
+    [1000, 2000, 3000]
+        .into_iter()
+        .map(|i| mixed[i % mixed.len()])
+        .sum()
+}
+
+fn part2() -> i64 {
+    let mut nodes = read_nodes();
+    for n in nodes.iter_mut() {
+        n.delta *= 811589153;
+    }
+
+    for _ in 0..10 {
+        mix(&mut nodes);
+    }
+    let mixed = to_final(&nodes);
+    [1000, 2000, 3000]
+        .into_iter()
+        .map(|i| mixed[i % mixed.len()])
+        .sum()
+}
+
+fn read_nodes() -> Vec<Node> {
+    let deltas = read_to_vec("data/day20.txt", |s| s.trim().parse::<i64>().unwrap());
+    let mut nodes = deltas
+        .into_iter()
+        .map(|d| Node {
+            delta: d,
+            prev: 0,
+            next: 0,
+        })
+        .collect::<Vec<Node>>();
+
+    for i in 0..nodes.len() {
+        let (prev, next) = match i {
+            0 => (nodes.len() - 1, 1),
+            _ if i == nodes.len() - 1 => (nodes.len() - 2, 0),
+            _ => (i - 1, i + 1),
+        };
+        nodes[i].prev = prev;
+        nodes[i].next = next;
+    }
+
+    nodes
 }
 
 pub fn run() {
-    let numbers = read_to_vec("data/day20.txt", |s| s.trim().parse::<i32>().unwrap());
     println!("== Day 20 ==");
-    println!("Part 1: {}", part1(&numbers))
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2())
 }
