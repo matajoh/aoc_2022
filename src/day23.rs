30b32abcd70bcc5c0b922b053abee8d1b1bf77aa
Matthew Johnson <matjoh@microsoft.com>
2022-12-26 01:12:53 +0000

    Day 23 refactor
    Signed-off-by: Matthew Johnson <matjoh@microsoft.com>

--- src/day23.rs
+++ src/day23.rs
@@ -1,17 +1,10 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::utils::read_to_vec;
-
-type Point = [i64; 2];
-
-#[derive(Hash, Eq, PartialEq, Copy, Clone)]
-struct Elf {
-    pos: Point,
-}
+use crate::{maths::Vec2, utils::read_to_vec};
 
 #[derive(Copy, Clone)]
 enum Action {
-    Move(Point),
+    Move(Vec2),
     Wait,
 }
 
@@ -26,31 +19,33 @@ const NE: Direction = 5;
 const SW: Direction = 6;
 const SE: Direction = 7;
 
-fn add(lhs: Point, rhs: Point) -> Point {
-    [lhs[0] + rhs[0], lhs[1] + rhs[1]]
-}
-
-const MOVES: [Point; 8] = [
-    [0, -1],
-    [0, 1],
-    [-1, 0],
-    [1, 0],
-    [-1, -1],
-    [1, -1],
-    [-1, 1],
-    [1, 1],
+const MOVES: [Vec2; 8] = [
+    Vec2 { x: 0, y: -1 },
+    Vec2 { x: 0, y: 1 },
+    Vec2 { x: -1, y: 0 },
+    Vec2 { x: 1, y: 0 },
+    Vec2 { x: -1, y: -1 },
+    Vec2 { x: 1, y: -1 },
+    Vec2 { x: -1, y: 1 },
+    Vec2 { x: 1, y: 1 },
 ];
 
-impl Elf {
-    fn propose_move(&self, elves: &HashSet<Point>, start: Direction) -> Action {
-        let mut moves = [[0, 0]; 8];
+trait Elf<T = Self> {
+    fn propose_move(&self, elves: &HashSet<T>, start: Direction) -> Action;
+}
+
+impl Elf for Vec2 {
+    fn propose_move(&self, elves: &HashSet<Vec2>, start: Direction) -> Action {
+        let mut moves = [Vec2::from(0, 0); 8];
         let mut occupied = [false; 8];
+        let mut any_occupied = false;
         for i in 0..8 {
-            moves[i] = add(self.pos, MOVES[i]);
+            moves[i] = self + &MOVES[i];
             occupied[i] = elves.contains(&moves[i]);
+            any_occupied = any_occupied || occupied[i];
         }
 
-        if !occupied.iter().any(|o| *o) {
+        if !any_occupied {
             return Action::Wait;
         }
 
@@ -72,7 +67,7 @@ impl Elf {
     }
 }
 
-fn read_elves() -> Vec<Elf> {
+fn read_elves() -> Vec<Vec2> {
     let lines = read_to_vec("data/day23.txt", |s| s.to_string());
     (0..lines.len())
         .flat_map(|y| {
@@ -80,96 +75,70 @@ fn read_elves() -> Vec<Elf> {
                 .chars()
                 .enumerate()
                 .filter_map(move |(x, c)| match c {
-                    '#' => Some(Elf {
-                        pos: [x as i64, y as i64],
-                    }),
+                    '#' => Some(Vec2::from(x as i32, y as i32)),
                     _ => None,
                 })
         })
         .collect()
 }
 
-fn update(elves: &Vec<Elf>, stage: Direction) -> (Vec<Elf>, bool) {
-    let mut counts = HashMap::new();
-    let occupied = elves.iter().map(|e| e.pos).collect();
-    let proposed = elves
-        .iter()
-        .map(|e| e.propose_move(&occupied, stage))
-        .collect::<Vec<Action>>();
-    for pos in &proposed {
-        if let Action::Move(newp) = pos {
-            if counts.contains_key(newp) {
-                counts.insert(newp, counts[newp] + 1);
-            } else {
-                counts.insert(newp, 1);
+fn update(elves: &mut Vec<Vec2>, stage: Direction) -> bool {
+    let mut is_proposed = HashMap::new();
+    let occupied = elves.iter().copied().collect();
+    let mut moved = 0;
+    for i in 0..elves.len() {
+        match elves[i].propose_move(&occupied, stage) {
+            Action::Move(newp) => {
+                if let Some((elf, oldp)) = is_proposed.insert(newp, (i, elves[i])) {
+                    if elves[elf] != oldp {
+                        elves[elf] = oldp;
+                        moved -= 1;
+                    }
+                } else {
+                    elves[i] = newp;
+                    moved += 1;
+                }
             }
+            _ => (),
         }
     }
 
-    let actions = proposed
-        .iter()
-        .map(|a| match a {
-            Action::Move(pos) => {
-                if counts[pos] > 1 {
-                    Action::Wait
-                } else {
-                    *a
-                }
-            }
-            _ => *a,
-        })
-        .collect::<Vec<Action>>();
-    let update = actions
-        .iter()
-        .zip(elves.iter())
-        .map(|(a, e)| match a {
-            Action::Move(newp) => Elf { pos: *newp },
-            Action::Wait => *e,
-        })
-        .collect();
-    let moved = proposed.iter().any(|a| match a {
-        Action::Move(_) => true,
-        _ => false,
-    });
-
-    (update, moved)
+    moved > 0
 }
 
-fn count_empty(elves: &Vec<Elf>) -> i64 {
-    let x_min = elves.iter().map(|e| e.pos[0]).min().unwrap();
-    let y_min = elves.iter().map(|e| e.pos[1]).min().unwrap();
-    let x_max = elves.iter().map(|e| e.pos[0]).max().unwrap() + 1;
-    let y_max = elves.iter().map(|e| e.pos[1]).max().unwrap() + 1;
-    (x_max - x_min) * (y_max - y_min) - elves.len() as i64
+fn count_empty(elves: &Vec<Vec2>) -> i32 {
+    let x_min = elves.iter().map(|e| e.x).min().unwrap();
+    let y_min = elves.iter().map(|e| e.y).min().unwrap();
+    let x_max = elves.iter().map(|e| e.x).max().unwrap() + 1;
+    let y_max = elves.iter().map(|e| e.y).max().unwrap() + 1;
+    (x_max - x_min) * (y_max - y_min) - elves.len() as i32
 }
 
-fn part1(start: &Vec<Elf>) -> i64 {
+fn part1(start: &Vec<Vec2>) -> i32 {
     let mut elves = start.iter().copied().collect();
     let mut stage = N;
     for _ in 0..10 {
-        let (next, moved) = update(&elves, stage);
+        let moved = update(&mut elves, stage);
         if !moved {
             break;
         }
 
-        elves = next;
         stage = (stage + 1) % 4;
     }
 
     count_empty(&elves)
 }
 
-fn part2(start: &Vec<Elf>) -> usize {
+fn part2(start: &Vec<Vec2>) -> usize {
     let mut elves = start.iter().copied().collect();
     let mut stage = N;
     let mut step = 1;
     loop {
-        let (next, moved) = update(&elves, stage);
+        let moved = update(&mut elves, stage);
         if !moved {
             break;
         }
 
-        elves = next;
         stage = (stage + 1) % 4;
         step += 1
     }
@@ -181,5 +150,5 @@ pub fn run() {
     let elves = read_elves();
     println!("== Day 23 ==");
     println!("Part 1: {}", part1(&elves));
-    println!("Part 2: {}", part2(&elves))
+    println!("Part 2: {}", part2(&elves));
 }
